7dd18330392d2d8c53c2f35524ee4182f462e5db
Henrique Coutinho <henrique.coutinho@engenharia.ufjf.br>
2023-11-06 22:38:30 -0300

    feat: Creating views

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+const path = require('path');
 const express = require('express');
 const morgan = require('morgan');
 const cors = require('cors');
@@ -14,7 +15,14 @@ const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
 const reviewRouter = require('./routes/reviewRoutes');
 
+const viewRouter = require('./routes/viewRoutes');
+
 const app = express();
+
+app.set('view engine', 'pug');
+app.set('views', path.join(__dirname, 'views'));
+
+app.use(express.static(path.join(__dirname, 'public')));
 
 // 1) Middlewares
 // Set Security HTTP Headers
@@ -63,8 +71,6 @@ app.use(
   })
 );
 
-app.use(express.static(`${__dirname}/public`));
-
 app.use((req, res, next) => {
   req.requestTime = new Date().toDateString();
   next();
@@ -72,6 +78,8 @@ app.use((req, res, next) => {
 
 app.use(compression());
 
+
+app.use('/', viewRouter);
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
 app.use('/api/v1/reviews', reviewRouter);
