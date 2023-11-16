0cc13093363a8afd6e88bf40bd1203be9f0ec6ad
Henrique Coutinho <henrique.coutinho@engenharia.ufjf.br>
2023-11-16 08:59:11 -0300

    feat: E-mail Templates

--- utils/email.js
+++ utils/email.js
@@ -1,33 +1,91 @@
 const nodemailer = require('nodemailer');
+const pug = require('pug');
+const htmlToText = require('html-to-text');
 
-const sendEmail = async (options) => {
-  //   const transporter = nodemailer.createTransport({
-  //     service: 'Gmail',
-  //     auth: {
-  //       user: process.env.EMAIL_USERNAME,
-  //       pass: process.env.EMAIL_PASSWORD,
-  //     },
-  //     // ACTIVE in Gmail "less secure app" option
-  //   });
+module.exports = class Email {
+  constructor(user, url) {
+    this.to = user.email;
+    this.firstName = user.name.split(' ')[0];
+    this.url = url;
+    this.from = `Henrique Coutinho <${process.env.EMAIL_FROM}>`;
+  }
 
-  const transporter = nodemailer.createTransport({
-    host: process.env.EMAIL_HOST,
-    port: process.env.EMAIL_PORT,
-    auth: {
-      user: process.env.EMAIL_USERNAME,
-      pass: process.env.EMAIL_PASSWORD,
-    },
-  });
+  newTransport() {
+    if (process.env.NODE_ENV === 'production') {
+      // sendgrid
+      return nodemailer.createTransport({
+        service: 'SendGrid',
+        auth: {
+          user: process.env.SENDGRID_USERNAME,
+          pass: process.env.SENDGRID_PASSWORD,
+        },
+      });
+    }
 
-  const mailOptions = {
-    from: 'Henrique Coutinho <teste@teste.com>',
-    to: options.email,
-    subject: options.subject,
-    text: options.message,
-    // html: options.message,
-  };
+    return nodemailer.createTransport({
+      host: process.env.EMAIL_HOST,
+      port: process.env.EMAIL_PORT,
+      auth: {
+        user: process.env.EMAIL_USERNAME,
+        pass: process.env.EMAIL_PASSWORD,
+      },
+    });
+  }
 
-  await transporter.sendMail(mailOptions);
-};
+  async send(template, subject) {
+    // render HTML based on a pug template
+    const html = pug.renderFile(`${__dirname}/../views/email/${template}.pug`, {
+      firstName: this.firstName,
+      url: this.url,
+      subject,
+    });
 
-module.exports = sendEmail;
+    // define email options
+    const mailOptions = {
+      from: this.from,
+      to: this.to,
+      subject,
+      html,
+      text: htmlToText.htmlToText(html),
+    };
+
+    // create a transport and send email
+    await this.newTransport().sendMail(mailOptions);
+  }
+
+  async sendWelcome() {
+    await this.send('welcome', 'Welcome to the Natours Family!');
+  }
+}
+
+// const sendEmail = async (options) => {
+//   //   const transporter = nodemailer.createTransport({
+//   //     service: 'Gmail',
+//   //     auth: {
+//   //       user: process.env.EMAIL_USERNAME,
+//   //       pass: process.env.EMAIL_PASSWORD,
+//   //     },
+//   //     // ACTIVE in Gmail "less secure app" option
+//   //   });
+
+//   const transporter = nodemailer.createTransport({
+//     host: process.env.EMAIL_HOST,
+//     port: process.env.EMAIL_PORT,
+//     auth: {
+//       user: process.env.EMAIL_USERNAME,
+//       pass: process.env.EMAIL_PASSWORD,
+//     },
+//   });
+
+//   const mailOptions = {
+//     from: 'Henrique Coutinho <teste@teste.com>',
+//     to: options.email,
+//     subject: options.subject,
+//     text: options.message,
+//     // html: options.message,
+//   };
+
+//   await transporter.sendMail(mailOptions);
+// };
+
+// module.exports = sendEmail;
